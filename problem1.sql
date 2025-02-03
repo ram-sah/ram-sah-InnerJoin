cd5dc54cff2b668a7708768715c440871103113d
Ram Sah <mohan2036@gmail.com>
2025-02-03 03:49:17 +0000

    update

--- problem1.sql
+++ problem1.sql
@@ -1,4 +1,6 @@
-SELECT student.id, student.student_name FROM class INNER JOIN student 
+SELECT student.id, student.student_name 
+FROM student 
+INNER JOIN class 
 ON class.class_title = student.class_title
 WHERE class.teacher_name = 'Ms. Lovelace';
 
